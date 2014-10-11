92031336aaeac5e5522b9ff24b6ae5f2a81ba38d
Karyn Armour <karynarmr33@gmail.com>
2014-10-11 15:18:53 -0500

    finished plugin exercises

--- fancybox/source/jquery.fancybox.js
+++ fancybox/source/jquery.fancybox.js
@@ -10,6 +10,26 @@
  *
  */
 
+
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none',
+		helpers		: {
+			title	: { type : 'inside' },
+			buttons	: {}
+		}
+	});
+});
+
+
 (function (window, document, $, undefined) {
 	"use strict";
 
